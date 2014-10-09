4754e13a078b8c8e65578783f38de9d797fd2b2a
James Young <pronoiac@gmail.com>
2014-10-08 18:20:10 -0700

    Refactoring a bit

--- dns-eyeball.rb
+++ dns-eyeball.rb
@@ -14,17 +14,26 @@ Resolv::DNS.open do |dns|
   puts "querying #{domain}..."
   ress = dns.getresources domain, Resolv::DNS::Resource::IN::ANY
   
+  byebug
+  
+  print_record(ress)
+end # /main
+
+def print_record(ress)
+  if ress.count == 0
+    print "No records returned!"
+    return false
+  end
+  
   case ress.first
   when Resolv::DNS::Resource::IN::CNAME
     puts "CNAME, points to..."
     p ress.first.name
-    puts "testing: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? }"
+    puts "[ prettier: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? } ]"
   when Resolv::DNS::Resource::IN::A
     puts "A, points to..."
     puts ress.first.address
   else
-    puts "First entry not recognized."
-    
+    # record not recognized...
   end
-  
-end
+end # /print_status
