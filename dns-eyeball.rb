cc087446229bb05c732f45f8c197c62e4a90c918
James Young <pronoiac@gmail.com>
2014-10-08 18:46:10 -0700

    Moving function, so it actually works. :)

--- dns-eyeball.rb
+++ dns-eyeball.rb
@@ -4,6 +4,22 @@ require "Resolv"
 require "byebug"
 
 
+def print_record(ress)
+  ress.each do |record| 
+    case ress.first
+    when Resolv::DNS::Resource::IN::CNAME
+      puts "CNAME, points to..."
+      p ress.first.name
+      puts "[ prettier: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? } ]"
+      return
+    when Resolv::DNS::Resource::IN::A
+      puts "A, points to..."
+      puts ress.first.address
+      return
+    end
+  end # /loop over records
+end # /print_status
+
 
 # die unless ARGV.length == 2
 puts "testing - google.com"
@@ -25,21 +41,4 @@ Resolv::DNS.open do |dns|
     end
   end # /host
 end # /main
-
-
-def print_record(ress)
-  ress.each do |record| 
-    case ress.first
-    when Resolv::DNS::Resource::IN::CNAME
-      puts "CNAME, points to..."
-      p ress.first.name
-      puts "[ prettier: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? } ]"
-      return
-    when Resolv::DNS::Resource::IN::A
-      puts "A, points to..."
-      puts ress.first.address
-      return
-    end
-  end # /loop over records
-end # /print_status
 
