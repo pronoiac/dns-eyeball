f5a72f73d418d76fc8d0fd6bd1d37d113e806dd4
James Young <pronoiac@gmail.com>
2014-10-08 18:45:20 -0700

    Iterating over records

--- dns-eyeball.rb
+++ dns-eyeball.rb
@@ -3,6 +3,8 @@
 require "Resolv"
 require "byebug"
 
+
+
 # die unless ARGV.length == 2
 puts "testing - google.com"
 p Resolv.getaddress "www.google.com"
@@ -10,11 +12,11 @@ p Resolv.getaddress "www.google.com"
 domain = ARGV[0] || "pronoiac.org"
 
 Resolv::DNS.open do |dns|
-  [domain, "www." + domain].each do |host|
+  # [domain, "www." + domain].each do |host|
+  [domain].each do |host|
 
     puts "querying #{host}..."
     ress = dns.getresources host, Resolv::DNS::Resource::IN::ANY
-    p ress
     
     if ress.count == 0
       puts "No records returned!"
@@ -24,20 +26,20 @@ Resolv::DNS.open do |dns|
   end # /host
 end # /main
 
+
 def print_record(ress)
-  case ress.first
-  when Resolv::DNS::Resource::IN::CNAME
-    puts "CNAME, points to..."
-    p ress.first.name
-    puts "[ prettier: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? } ]"
-  when Resolv::DNS::Resource::IN::A
-    puts "A, points to..."
-    puts ress.first.address
-  else
-    # record not recognized...
-    puts "Record not recognized: "
-    p ress.first
-    puts "more following:"
-    p ress
-  end
+  ress.each do |record| 
+    case ress.first
+    when Resolv::DNS::Resource::IN::CNAME
+      puts "CNAME, points to..."
+      p ress.first.name
+      puts "[ prettier: #{ress.first.name.to_s}#{ '.' if ress.first.name.absolute? } ]"
+      return
+    when Resolv::DNS::Resource::IN::A
+      puts "A, points to..."
+      puts ress.first.address
+      return
+    end
+  end # /loop over records
 end # /print_status
+
